257f332ea8a9d509df0f2e9f8b4b03758f79c261
55g5g5yyu7-dot <55g5g5yyu7@gmail.com>
2026-02-23 15:09:24 +0300

    fix: preload extratextareas mysql model classes in verify step

--- core/components/extratextareas/processors/mgr/field/remove.class.php
+++ core/components/extratextareas/processors/mgr/field/remove.class.php
@@ -2,6 +2,15 @@
 
 class ExtraTextAreasFieldRemoveProcessor extends modObjectRemoveProcessor
 {
+
+    public function initialize()
+    {
+        $corePath = $this->modx->getOption('extratextareas.core_path', null, $this->modx->getOption('core_path') . 'components/extratextareas/');
+        require_once $corePath . 'src/ExtraTextAreas.php';
+        new ExtraTextAreas($this->modx);
+
+        return parent::initialize();
+    }
     public $classKey = ExtraTextAreasField::class;
     public $objectType = 'extratextareas.field';
 }
